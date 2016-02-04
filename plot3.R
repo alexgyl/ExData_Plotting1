a0ba92400050c4cc38a66d658d4cb379d46bf714
alexgyl <gyl@u.nus.edu>
2016-02-04 13:32:39 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -22,11 +22,11 @@ sub$DateTime = paste(sub$Date,sub$Time)
 sub$DateTime = ymd_hms(sub$DateTime)
 
 ## Plotting of line graph with datetime and colors
+png(filename = "plot3.png",width = 480,height = 480)
 plot(x = sub$DateTime, y= sub$Sub_metering_1,type = "n",ylab = "Energy sub metering", xlab = "")
 lines(x = sub$DateTime,y = sub$Sub_metering_1,col = "black")
 lines(x = sub$DateTime,y = sub$Sub_metering_2,col = "red")
 lines(x = sub$DateTime,y = sub$Sub_metering_3,col = "blue")
 legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col = c("black","red","blue"),lty = 1,cex = 0.7)
 
-dev.copy(png,file = "plot3.png")
 dev.off()
