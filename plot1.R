9a43301943a71be9882716f766d41c590cffed94
alexgyl <gyl@u.nus.edu>
2016-02-04 13:31:56 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -15,7 +15,6 @@ sub = filter(data,(Date == ymd("2007-02-01")) | (Date == ymd("2007-02-02")))
 sub$Global_active_power = as.numeric(sub$Global_active_power)
 
 ## Histogram for plot 1
+png(filename = "plot1.png",width = 480,height = 480)
 hist(sub$Global_active_power,main = "Global Active Power",col = "red",xlab = "Global Active Power (kilowatts)",ylab = "Frequency")
-## Copying plot to png file
-dev.copy(png,file = "plot1.png")
 dev.off()
