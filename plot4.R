35a663240b458ea170b3743866e57277851959b5
alexgyl <gyl@u.nus.edu>
2016-02-04 13:32:56 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -23,7 +23,8 @@ sub$DateTime = paste(sub$Date,sub$Time)
 sub$DateTime = ymd_hms(sub$DateTime)
 
 ## Plotting all 4 plots
-par(mfrow = c(2,2),mar = c(5,2,2,1))
+png(filename = "plot4.png",width = 480,height = 480)
+par(mfrow = c(2,2))
 ## Rows fill up row-wise
 ## Plot 1
 plot(y = sub$Global_active_power,x = sub$DateTime,type = "n",ylab = "Global Active Power", xlab = "")
@@ -36,12 +37,11 @@ plot(x = sub$DateTime, y= sub$Sub_metering_1,type = "n",ylab = "Energy sub meter
 lines(x = sub$DateTime,y = sub$Sub_metering_1,col = "black")
 lines(x = sub$DateTime,y = sub$Sub_metering_2,col = "red")
 lines(x = sub$DateTime,y = sub$Sub_metering_3,col = "blue")
-legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col = c("black","red","blue"),lty = 1,cex = 0.5,bty = "n")
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col = c("black","red","blue"),lty = 1,cex = 0.7,bty = "n")
 
 ## Plot 4
 plot(y = sub$Global_reactive_power,x = sub$DateTime,type = "n", xlab = "datetime",ylab = "Global_reactive_power")
 lines(x = sub$DateTime,y = sub$Global_reactive_power)
 
-dev.copy(png,file = "plot4.png")
 dev.off()
 
